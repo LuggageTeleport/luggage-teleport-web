c1fcde7f9e4d62cf25bd3b1ce76976ed563526e2
Luthfir <luthfirrahman04@gmail.com>
2017-12-07 04:30:55 +0700

    adding new page for booking and updating the UI

--- src/App.js
+++ src/App.js
@@ -13,21 +13,54 @@ import SectionFive from './components/section_five';
 import SectionSix from './components/section_six';
 import Footer from './components/footer';
 
+import AirportToHotel from './components/airport_to_hotel';
+import HotelToAirport from './components/hotel_to_airport';
+import HotelToHotel from './components/hotel_to_hotel';
+import AirportToAirport from './components/airport_to_airport';
+
 
 
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Header />
-        <SectionOne />
-        <SectionTwo />
-        <SectionThree />
-        <SectionFour />
-        <SectionFive />
-        <SectionSix />
-        <Footer />
+      <div>
+        <div>
+          <Container fluid style={{ lineHeight: '35px' }}>
+            <Row>
+              <Col  md={9} push={{ md: 3 }}>
+                <div>
+                  <Header />
+                  <SectionOne />
+                  <SectionTwo />
+                  <SectionThree />
+                  <SectionFour />
+                  <SectionFive />
+                  <SectionSix />
+                  <Footer />
+                </div>
+              </Col>
+              <Col md={3} pull={{ md: 9 }}>
+                <div style={{ padding: '10px' }}>
+                  <Tabs defaultActiveKey={1} animation={false} id="noanim-tab-example">
+                    <Tab eventKey={1} title="Airport-Hotel">
+                      <AirportToHotel />
+                    </Tab>
+                    <Tab eventKey={2} title="Hotel-Airport">
+                      <HotelToAirport />
+                    </Tab>
+                    <Tab eventKey={3} title="Hotel-Hotel">
+                      <HotelToHotel />
+                    </Tab>
+                    <Tab eventKey={4} title="Airport-Airport">
+                      <AirportToAirport />
+                    </Tab>
+                  </Tabs>
+                </div>
+              </Col>
+            </Row>
+          </Container>
+        </div>
       </div>
     );
   }
