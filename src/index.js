83b6ebb179ba04cc4b15f916f9db8efc60537c5b
Luthfir <luthfirrahman04@gmail.com>
2017-12-18 16:11:38 +0700

    adding store, reducer, LogUser and Provider

--- src/index.js
+++ src/index.js
@@ -4,17 +4,25 @@ import './index.css';
 import App from './App';
 import Login from './components/login';
 import Register from './components/register';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import { Route, BrowserRouter as Router } from 'react-router-dom';
+import { LogUser } from './actions';
+import reducer from './reducers';
 
 import registerServiceWorker from './registerServiceWorker';
 
+const store = createStore(reducer);
+
 ReactDOM.render(
-    <Router>
-        <div>
-            <Route exact path="/" component={Login} />
-            <Route path="/register" component={Register} />
-            <Route path="/home" component={App} />
-        </div>
-    </Router>
+    <Provider store={store}>
+        <Router>
+            <div>
+                <Route exact path="/login" component={Login} />
+                <Route exact path="/register" component={Register} />
+                <Route exact path="/" component={App} />
+            </div>
+        </Router>
+    </Provider>
     , document.getElementById('root'));
 registerServiceWorker();
