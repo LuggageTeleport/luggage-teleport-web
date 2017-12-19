0f1dc6b3129b5d138052728cd3bd658776fe0702
Luthfir <luthfirrahman04@gmail.com>
2017-12-19 09:09:45 +0700

    minor change

--- src/index.js
+++ src/index.js
@@ -1,13 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import Login from './components/login';
-import Register from './components/register';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
-import { Route, BrowserRouter as Router } from 'react-router-dom';
-import { LogUser } from './actions';
+import Routes from './routes';
 import reducer from './reducers';
 
 import registerServiceWorker from './registerServiceWorker';
@@ -16,13 +12,7 @@ const store = createStore(reducer);
 
 ReactDOM.render(
     <Provider store={store}>
-        <Router>
-            <div>
-                <Route exact path="/login" component={Login} />
-                <Route exact path="/register" component={Register} />
-                <Route exact path="/" component={App} />
-            </div>
-        </Router>
+        <Routes />
     </Provider>
     , document.getElementById('root'));
 registerServiceWorker();
