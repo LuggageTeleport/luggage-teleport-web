27aaefa3a33a8114e1c5e1d2eec1306dbe9b80a1
Luthfir <luthfirrahman04@gmail.com>
2017-12-21 04:29:08 +0700

    adding function signUpUser for register user data(still any bugs)

--- src/components/register.js
+++ src/components/register.js
@@ -1,12 +1,46 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { signUpUser } from '../aws_cognito';
+import createHistory from 'history/createBrowserHistory';
 import '../App.css';
 
 class Register extends Component {
 
     constructor(props) {
         super(props);
+
+        this.state = {
+            name: '',
+            email: '',
+            phone_number: '',
+            password: '',
+            confirmPassword: '',
+            newUser: null
+        }
     }
+
+    validateForm() {
+        return (
+            this.state.email.length > 0 &&
+            this.state.password.length > 8 &&
+            this.state.password === this.state.confirmPassword
+        );
+    }
+
+    signUp() {
+        const history = createHistory();
+        const { name, phone_number, email, password } = this.state;
+        signUpUser(email, name, phone_number, password)
+            .then((result) => {
+                history.push('/verify');
+                localStorage.setItem('User_Email', email)
+                console.log(result)
+            })
+            .catch((err) => {
+                console.log(err)
+            })
+    }
+
 
     render() {
         return (
@@ -18,6 +52,7 @@ class Register extends Component {
                             <input
                                 className="form-control"
                                 type="text"
+                                onChange={e => this.setState({ name: e.target.value })}
                                 placeholder="Your Fullname" required />
                         </div>
 
@@ -25,6 +60,7 @@ class Register extends Component {
                             <input
                                 className="form-control"
                                 type="email"
+                                onChange={e => this.setState({ email: e.target.value })}
                                 placeholder="Your Active Email"
                                 style={{ marginTop: '10px' }} required />
                         </div>
@@ -33,6 +69,7 @@ class Register extends Component {
                             <input
                                 className="form-control"
                                 type="text"
+                                onChange={e => this.setState({ phone_number: e.target.value })}
                                 style={{ marginTop: '10px' }}
                                 placeholder="Your Phone Number" required />
                         </div>
@@ -41,14 +78,31 @@ class Register extends Component {
                             <input
                                 className="form-control"
                                 type="password"
+                                onChange={e => this.setState({ password: e.target.value })}
                                 placeholder="Your Password"
                                 style={{ marginTop: '10px' }} required />
                         </div>
 
-                        <button className="btn btn-lg btn-primary">Register</button>
-                        
-                        <div style={{marginTop: '3em'}}>
-                            <p><strong>Already Have an Account?</strong><Link to="/login"> <a style={{color: 'white'}}>Sign In</a></Link></p>
+                        <div className="form-group">
+                            <input
+                                className="form-control"
+                                type="password"
+                                onChange={e => this.setState({ confirmPassword: e.target.value })}
+                                placeholder="Confirm your Password"
+                                style={{ marginTop: '10px' }} required />
+                        </div>
+
+                        <button
+                            className="btn btn-lg btn-primary"
+                            type="submit"
+                            disabled={!this.validateForm()}
+                            onClick={this.signUp()}
+                        >
+                            Register
+                        </button>
+
+                        <div style={{ marginTop: '3em' }}>
+                            <p><strong>Already Have an Account?</strong><Link to="/login"> <a style={{ color: 'white' }}>Sign In</a></Link></p>
                         </div>
                     </form>
                 </div>
