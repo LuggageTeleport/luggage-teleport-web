37de8411d9df42718e9fd776a589ac13a8fd188b
Luthfir <luthfirrahman04@gmail.com>
2017-12-28 14:42:39 +0700

    change state to action (fixing bugs)

--- src/reducers/reducer_book_data.js
+++ src/reducers/reducer_book_data.js
@@ -3,7 +3,7 @@ import { PASSING_BOOK_DATA } from '../constants';
 export default (state = [], action) => {
     switch (action.type) {
         case PASSING_BOOK_DATA:
-            const { BookData } = state;
+            const { BookData } = action;
             return BookData;
         default:
             return state;
